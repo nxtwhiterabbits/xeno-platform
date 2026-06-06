69a7446ce82eee42392b5c3db5a1cedad63de11a
nxtwhiterabbits <nxtwhiterabbits@gmail.com>
2026-06-06 14:32:10 +0800

    Update auth.js

--- core/auth.js
+++ core/auth.js
@@ -1,6 +1,6 @@
 async function login(username, password) {
 
-    console.log("[AUTH] LOGIN START");
+    console.log("[LOGIN] START");
 
     const res = await request("/api/auth/login.php", {
         body: {
@@ -9,27 +9,21 @@ async function login(username, password) {
         }
     });
 
-    console.log("[AUTH] RESPONSE", res);
+    console.log("[LOGIN RESPONSE]", res);
 
     if (res.status !== "success") {
         throw new Error(res.message || "Login failed");
     }
 
     localStorage.setItem("token", res.token);
-    localStorage.setItem("refresh", res.refresh_token || "");
-    localStorage.setItem("user", res.username || "");
-    localStorage.setItem("xid", res.xid || "");
+    localStorage.setItem("refresh", res.refresh_token);
+    localStorage.setItem("user", res.username);
+    localStorage.setItem("xid", res.xid);
 
     window.location.href = "./wallet.html";
-
-    return res;
 }
 
 function logout() {
     localStorage.clear();
     window.location.href = "./login.html";
-}
-
-function isLoggedIn() {
-    return !!localStorage.getItem("token");
 }
